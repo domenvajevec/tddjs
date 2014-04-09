2c3d93fd29980124d5eb1cff7b1c8eccc12db800
domenvajevec <domenvajevec@gmail.com>
2014-04-09 13:00:17 -0700

    add continuous integration script

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,7 @@
 /*global desc, task, jake, fail, complete */
 (function() {
     "use strict";
-
+    desc("Build and test");
     task("default", ["lint"]);
 
     desc("Lint everything");
@@ -12,9 +12,22 @@
         files.include("**/*.js");
         files.exclude("node_modules");
         var options = nodeLintOptions();
-        lint.validateFileList(files.toArray(), options, {});
+        var passed = lint.validateFileList(files.toArray(), options, {});
+        if (!passed) fail("Lint failed");
     });
 
+    desc("Integrate");
+    task("Integrate", ["default"], function(){
+        console.log("1. Make sure 'git status' is clean.");
+        console.log("2. Build on the integration box.");
+        console.log("   a. Walk over to integration box.");
+        console.log("   b. 'git pull'");
+        console.log("   c. 'jake'");
+        console.log("   d. If jake fails, stop! Try again after fixing the issue.");
+        console.log("3. 'git checkout integration'");
+        console.log("4. 'git merge master --no-ff --log'");
+        console.log("5. 'git checkout master'");
+    });
     function nodeLintOptions() {
         return {
             bitwise:true,

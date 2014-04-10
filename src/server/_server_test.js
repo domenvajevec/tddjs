a830c044d02b9d82b1b282fcc6a66fc328528a97
domenvajevec <domenvajevec@gmail.com>
2014-04-09 20:56:01 -0700

    add server response test

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,7 +1,34 @@
 "use strict";
 var server = require('./server.js');
-var assert = require("assert");
-exports.testNothing = function(test){
-    test.equals(3,server.number(),"number");
-    test.done ();
+var http = require('http');
+
+
+exports.tearDown = function(done){
+    server.stop(function(){
+//        console.log("teardown");
+        done();
+    });
+
+};
+
+
+exports.testHelloWorld = function(test){
+    server.start(8080);
+    var request = http.get("http://localhost:8080", function(response){
+        var receivedData = false;
+        response.setEncoding('utf-8');
+
+        test.equals(200, response.statusCode, "status code");
+        response.on("data", function(chunk) {
+            receivedData = true;
+            test.equals("Hello World", chunk, "response text");
+        });
+        response.on("end", function() {
+            test.ok(receivedData, "should have received response data");
+            test.done();
+        });
+//        response.on("data", function(){
+//            test.done();
+//        });
+    });
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,26 @@
 "use strict";
 
-console.log("This is a server");
-exports.number = function(){
-    return 3;
+var http = require("http");
+var server;
+
+exports.start = function(){
+
+    server = http.createServer();
+
+    server.on("request", function(req,res){
+        console.log("request called");
+        res.statusCode = 200;
+        res.end("Hello World");
+    });
+    server.listen(8080);
+    console.log("server started");
 };
+
+exports.stop = function(callback){
+    server.close(callback);
+};
+
+
+
+
 

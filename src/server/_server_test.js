b6778a01da6b8e49dafb5d3a6c6781fec45f9bd7
domenvajevec <domenvajevec@gmail.com>
2014-04-09 17:12:44 -0700

    add demo test

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -2,6 +2,6 @@
 var server = require('./server.js');
 var assert = require("assert");
 exports.testNothing = function(test){
-    assert.equal(3,server.number(),"number");
+    test.equals(3,server.number(),"number");
     test.done ();
 };

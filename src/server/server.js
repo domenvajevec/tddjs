849224b36d47cf7c1f9b591d086a42af2ca7a22d
domenvajevec <domenvajevec@gmail.com>
2014-04-14 14:19:14 -0700

    add server servesa file test

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,23 @@
 "use strict";
 
 var http = require("http");
+var fs = require("fs");
 var server;
 
-exports.start = function(portNumber){
+exports.start = function(htmlFileToServe, portNumber) {
     if(!portNumber) throw "port number is required";
-    server = http.createServer();
 
-    server.on("request", function(req,res){
-        res.end("Hello World");
+    server = http.createServer();
+    server.on("request", function(request, response) {
+        fs.readFile(htmlFileToServe, function (err, data) {
+            if (err) throw err;     //TODO: fix me
+            response.end(data);
+        });
     });
     server.listen(portNumber);
 };
 
-exports.stop = function(callback){
+exports.stop = function(callback) {
     server.close(callback);
 };
 
